6939e7bba290bc2ae7c2ad8defd5f10a40e69c9a
tomasztaw <tomasz.wegrzyn.wro@gmail.com>
2023-09-01 15:11:30 +0200

    Commit przed scaleniem

--- src/main/resources/db/migration/V2_0__init_doctor_table.sql
+++ src/main/resources/db/migration/V2_0__init_doctor_table.sql
@@ -6,8 +6,7 @@ VALUES ('Alojzy', 'Kowalski', 'Laryngolog', 'alojzkowalski@eclinic.pl', '+48 120
 ('Wacław', 'Piątkowski', 'Gastrolog', 'wacek@eclinic.pl', '+48 120 130 150'),
 ('Aleksander', 'Newski', 'Gastrolog', 'oleknew@eclinic.pl', '+48 120 130 152'),
 ('Urszula', 'Nowakowska', 'Lekarz rodzinny', 'ulala@eclinic.pl', '+48 120 130 158'),
-('Paweł', 'Stękała', 'Podolog', 'p.stekala@eclinic.pl', '+48 120 130 160'),
---poprawić na stomatologa
+('Paweł', 'Stękała', 'Stomatolog', 'p.stekala@eclinic.pl', '+48 120 130 160'),
 ('Magdalena', 'Kraska', 'Ortopeda', 'm.kraska@eclinic.pl', '+48 120 130 161'),
 ('Joanna', 'Wojtyniak', 'Chirurg', 'j.wojtyniak@eclinic.pl', '+48 120 130 162'),
 ('Małgorzata', 'Figurska', 'Neurolog', 'm.figurska@eclinic.pl', '+48 120 130 163'),
